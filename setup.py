5353b6ee74bc219041856313ec4f84bf757ae71e
Grzegorz Chrupala <g.chrupala@uvt.nl>
2019-03-05 14:51:11 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+setup(
+    name='ursa',
+    version='0.1.0',
+    url='https://github.com/gchrupala/ursa',
+    packages=find_packages(exclude=['examples']),
+    license='Apache License 2.0',
+    description='Neural representation analysis with representational similarity'
+)
+
