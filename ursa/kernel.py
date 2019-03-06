b1dfe2afd6cef0db38cf7e91d87763a9ccb9e5f2
Grzegorz Chrupala <g.chrupala@uvt.nl>
2019-03-06 14:17:49 +0100

    tree kernel code

---
+++ ursa/kernel.py
@@ -0,0 +1,69 @@
+from functools import reduce
+from typing import Sequence, List, Any, Union, TypeVar, Iterable
+from typing_extensions import Protocol
+
+class Labeled(Protocol):
+    def label(self) -> str: ...
+
+class TreeLike(Sequence['TreeLike'], Labeled): ...
+
+# These functions work with nltk.tree.Tree
+
+def label(n: Union[Labeled, str]) -> str:
+    if isinstance(n, str):
+        return n
+    else:
+        return n.label()
+
+def leaf(n: TreeLike) -> bool:
+    return isinstance(n, str)
+
+def children(n: TreeLike) -> Sequence[TreeLike]:
+    if isinstance(n, str):
+        return []
+    else:
+        return n[:]
+
+class Kernel:
+    """Class to hold configuration of the kernel function."""
+    def __init__(self, label=label, leaf=leaf, children=children, ignore_terminals=False):
+        self.label = label
+        self.leaf = leaf
+        self.children = children
+        self.ignore_terminals = ignore_terminals
+
+    def subtrees(self, t: TreeLike) -> Iterable[TreeLike]:
+        """Yields all subtrees of a tree t."""
+        if leaf(t):
+            pass
+        else:
+            yield t
+            for c in self.children(t):
+                yield from self.subtrees(c)
+
+    def preterm(self, t: TreeLike) -> bool:
+        """Returns True if node t is a pre-terminal."""
+        return len(self.children(t)) == 1 and self.leaf(self.children(t)[0])
+    
+    def production(self, t: TreeLike) -> List[str]:
+        """Returns the productiona at node t, i.e. the list of children's labels."""
+        return [ self.label(c) for c in self.children(t) ]
+
+    def C(self, n1: TreeLike, n2: TreeLike, alpha: float=1.0) -> float:
+        # both nodes are preterminals and have same productions
+        if self.preterm(n1) and self.preterm(n2) and self.label(n1) == self.label(n2) and (self.production(n1) == self.production(n2) or self.ignore_terminals):
+            return alpha
+        # both nodes are non-terminals and have same productions
+        elif not self.preterm(n1) and not self.preterm(n2) and self.label(n1) == self.label(n2) and self.production(n1) == self.production(n2):
+            return alpha * product(1 + self.C(n1[i], n2[i], alpha=alpha) for i in range(len(self.children(n1))))
+        else:
+            return 0
+
+    def __call__(self, t1: TreeLike, t2: TreeLike, alpha: float=1.0):
+        """Returns the number of shared tree fragments between trees t1 and t2, discounted by alpha."""
+        N = sum(self.C(n1, n2, alpha=alpha) for n1 in self.subtrees(t1) for n2 in self.subtrees(t2))
+        return N
+
+def product(xs: Iterable[float]) -> float:
+    return reduce(lambda a, b: a*b, xs)
+
