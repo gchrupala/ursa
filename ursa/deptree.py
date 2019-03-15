3bb554262422d840f5721ca39655a71514f39cfb
Grzegorz Chrupala <g.chrupala@uvt.nl>
2019-03-15 17:17:38 +0100

    Remove deprel.leaf

--- ursa/deptree.py
+++ ursa/deptree.py
@@ -6,9 +6,6 @@ from collections import OrderedDict
 def label(n: C.TokenTree) -> str:
     return n.token['deprel']
 
-def leaf(n: C.TokenTree) -> bool:
-    return len(n.children) == 0
-
 def children(n: C.TokenTree) -> Sequence[C.TokenTree]:
     return n.children
 
