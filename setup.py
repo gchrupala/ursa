e1e35eea7c42a0bc7f22ffc6e7df6e9eac5ba7b0
Grzegorz Chrupała <pitekus@gmail.com>
2020-07-17 09:58:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ setup(
     name='ursa',
     version='0.3.1',
     url='https://github.com/gchrupala/ursa',
-    packages=['ursa'],
     license='Apache License 2.0',
     description='Neural representation analysis with representational similarity',
     packages=find_packages(exclude='test'),
